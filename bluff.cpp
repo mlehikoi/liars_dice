d5791795b587fee9df836803319f8384555e92ca
Miikka Lehikoinen <mlehikoi@yahoo.com>
2017-09-14 23:34:39 +0300

    Initial commit

---
+++ bluff.cpp
@@ -0,0 +1,56 @@
+#include "crow.h"
+
+#include <string>
+#include <sstream>
+#include <unordered_map>
+#include <boost/uuid/uuid.hpp>            // uuid class
+#include <boost/uuid/uuid_generators.hpp> // generators
+#include <boost/uuid/uuid_io.hpp>         // streaming operators etc.
+
+class Games
+{
+public:
+    std::unordered_map<std::string, int> map_;
+
+    void addGame(const std::string& id)
+    {
+        map_.insert({id, 0});
+        std::cout << map_.size() << std::endl;
+    }
+};
+
+auto& games()
+{
+    static Games games_;
+    return games_;
+}
+int main()
+{
+    crow::SimpleApp app;
+
+    CROW_ROUTE(app, "/")([](){
+        return "Hello world";
+    });
+    
+    CROW_ROUTE(app, "/api/create")([]{
+        static auto generator = boost::uuids::random_generator();
+        const auto uuid = generator();
+        std::stringstream ss;
+        ss << uuid;
+        games().addGame(ss.str());
+        ss << " " << games().map_.size();
+        return ss.str();
+    });
+    
+    CROW_ROUTE(app, "/api/list")([]{
+        std::stringstream ss;
+        ss << "Games" << std::endl;
+        for (const auto& game : games().map_)
+        {
+            ss << "..." << game.first << std::endl;
+        }
+        return ss.str();
+    });
+
+    app.port(18080).multithreaded().run();
+}
