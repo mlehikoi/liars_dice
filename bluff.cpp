22401d5bb0d15c2d2e465cae54b36549cdc24cab
Miikka Lehikoinen <miikka.lehikoinen@ge.com>
2017-09-27 23:18:22 +0300

    Support game

--- bluff.cpp
+++ bluff.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <sstream>
 #include <unordered_map>
+#include <unordered_set>
 #include <boost/uuid/uuid.hpp>            // uuid class
 #include <boost/uuid/uuid_generators.hpp> // generators
 #include <boost/uuid/uuid_io.hpp>         // streaming operators etc.
@@ -30,17 +31,22 @@ auto& games()
     static Games games_;
     return games_;
 }
-
-static std::unordered_map<std::string, std::string> players_;
+struct PlayerInfo
+{
+    std::string name_;
+    std::string game_;
+};
+static std::unordered_map<std::string, PlayerInfo> players_;
+static std::unordered_set<std::string> games_;
 
 int main()
 {
     std::ifstream is("players.dat");
     while (is.good())
     {
-        std::string id, name;
-        is >> id >> name;
-        players_[id] = name;
+        std::string id, name, game;
+        is >> id >> name >> game;
+        players_[id] = {name, game};
     }
     
     crow::SimpleApp app;
@@ -58,17 +64,17 @@ int main()
             const std::string name = j["name"].s();
             for (const auto& idName : players_)
             {
-                if (idName.second == name)
+                if (idName.second.name_ == name)
                 {
                     response["success"] = false;
                     return response;
                 }
             }
             const auto id = uuid();
-            players_.insert({id, name});
+            players_.insert({id, {name, ""}});
             std::ofstream os("players.dat");
             for (const auto& idName : players_)
-                os << idName.first << "\t" << idName.second << endl;
+                os << idName.first << "\t" << idName.second.name_ << "\t" << idName.second.game_ << endl;
             response["success"] = true;
             response["playerId"] = id;
             return response;
@@ -89,12 +95,31 @@ int main()
                 {
                     response["success"] = true;
                     response["playerId"] = id;
-                    response["name"] = it->second;
+                    response["name"] = it->second.name_;
+                    response["game"] = it->second.game_;
                     return crow::response{response};
                 }
             }
             response["success"] = false;
             return crow::response(response);
+        }
+    );
+    
+    CROW_ROUTE(app, "/api/newGame")
+        .methods("POST"_method)
+        ([](const crow::request& req)
+        {
+            crow::json::wvalue response;
+            auto j = crow::json::load(req.body);
+            const std::string id = j["id"].s();
+            const std::string game = j["game"].s();
+            if (games_.insert(game).second)
+            {
+                response["success"] = true;
+                return response;
+            }
+            response["success"] = false;
+            return response;
         }
     );
 
