bcbce016f2ff827e8bdc0daa0e6e09fbfc72997a
Miikka Lehikoinen <mlehikoi@yahoo.com>
2017-10-23 00:27:19 +0300

    ESLint fixes

--- static/dice.js
+++ static/dice.js
@@ -1,15 +1,52 @@
-///*global
-//browser:true,
-//$, JQuery:true,
-//alert, confirm, console, Debug, opera, prompt, WSH
-//*/
-/*jslint devel: true */
+/*global $, console*/
+/*eslint no-console: ["error", { allow: ["log"] }] */
+///*eslint no-unused-vars: ["error", { "vars": "local" }]*/
+/*global window*/
 
-var userKey = "";
 var userName = "";
 var userId = "";
 
-function login(name) {
+function getStatus() {
+    'use strict';
+    console.log(userId);
+    $.ajax({
+        type: "POST",
+        url: "/api/status",
+        data: JSON.stringify({id: userId}),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (data) {
+            console.log(data);
+            if (data.success) {
+                if (data.hasOwnProperty("game")) {
+                    console.log(data.game);
+                    $("#welcomeMessage").html(data.name + ", waiting for others to join the game. Click start when you're" +
+                                              " ready to start the game.");
+                    $("#welcomeMessage").removeClass("hidden");
+                    $("#Login").addClass('hidden');
+                    $("#SetupCreate").addClass('hidden');
+                } else {
+                    $("#welcomeMessage").html("Welcome, " + data.name + "." +
+                                              " Start up a new game or select an existing" +
+                                              " game to join.");
+                    $("#welcomeMessage").removeClass("hidden");
+                    $("#Login").addClass('hidden');
+                    $("#SetupCreate").removeClass('hidden');
+                }
+            } else {
+                $("#Login").removeClass('hidden');
+                $("#SetupCreate").addClass('hidden');
+            }
+        },
+        error: function () {
+            //alert(errMsg);
+            console.log("error");
+            //console.log(errMsg);
+        }
+    });
+}
+
+function login(name) { // eslint-disable-line no-unused-vars
     'use strict';
     console.log("Fetching " + name);
     $.ajax({
@@ -35,7 +72,7 @@ function login(name) {
             }
             console.log(data);
         },
-        error: function (xhr, statusText, err) {
+        error: function () {
             //alert(errMsg);
             console.log("error");
             //console.log(errMsg);
@@ -43,7 +80,7 @@ function login(name) {
     });
 }
 
-function createGame(name) {
+function createGame(name) { // eslint-disable-line no-unused-vars
     'use strict';
     $.ajax({
         type: "POST",
@@ -61,7 +98,7 @@ function createGame(name) {
             }
             console.log(data);
         },
-        error: function (xhr, statusText, err) {
+        error: function () {
             //alert(errMsg);
             console.log("error");
             //console.log(errMsg);
@@ -69,43 +106,3 @@ function createGame(name) {
     });
 }
 
-function getStatus()
-{
-    console.log(userId);
-    $.ajax({
-        type: "POST",
-        url: "/api/status",
-        data: JSON.stringify({id: userId}),
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        success: function (data) {
-            console.log(data);
-            if (data.success) {
-                if (data.hasOwnProperty("game")) {
-                    console.log(data.game);
-                    $("#welcomeMessage").html(data.name + ", waiting for others to join the game. Click start when you're" +
-                                              " ready to start the game.");
-                    $("#welcomeMessage").removeClass("hidden");
-                    $("#Login").addClass('hidden');
-                    $("#SetupCreate").addClass('hidden');
-                }
-                else {
-                    $("#welcomeMessage").html("Welcome, " + data.name + "." +
-                                              " Start up a new game or select an existing" +
-                                              " game to join.");
-                    $("#welcomeMessage").removeClass("hidden");
-                    $("#Login").addClass('hidden');
-                    $("#SetupCreate").removeClass('hidden');
-                }
-            } else {
-                $("#Login").removeClass('hidden');
-                $("#SetupCreate").addClass('hidden');
-            }
-        },
-        error: function (xhr, statusText, err) {
-            //alert(errMsg);
-            console.log("error");
-            //console.log(errMsg);
-        }
-    });
-}
