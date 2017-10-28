ddc44a159470935e9b1653d00787e02aefb6673e
Miikka Lehikoinen <mlehikoi@yahoo.com>
2017-10-28 14:19:26 +0300

    Game to cpp

--- game.cpp
+++ game.cpp
@@ -1,1 +1,280 @@
-Dice defaultDice_;
+#include "game.hpp"
+
+namespace dice {
+
+int Game::getOffset() const
+{
+    std::vector<int> commonHand;
+    for (const auto p : players_)
+    {
+        commonHand.insert(commonHand.end(), p.hand().begin(), p.hand().end());
+    }
+    return currentBid_.challenge(commonHand);
+}
+
+const char* Game::toString(State state)
+{
+    switch (state)
+    {
+    case GAME_NOT_STARTED: return "GAME_NOT_STARTED";
+    case GAME_STARTED: return "GAME_STARTED";
+    case ROUND_STARTED: return "ROUND_STARTED";
+    case CHALLENGE: return "CHALLENGE";
+    case GAME_FINISHED: return "GAME_FINISHED";
+    }
+}
+
+Game::State Game::fromString(const std::string& str)
+{
+    if (str == "GAME_NOT_STARTED") return GAME_NOT_STARTED;
+    if (str == "GAME_STARTED") return GAME_STARTED;
+    if (str == "ROUND_STARTED") return ROUND_STARTED;
+    if (str == "CHALLENGE") return CHALLENGE;
+    if (str == "GAME_FINISHED") return GAME_FINISHED;
+    return GAME_NOT_STARTED;
+}
+
+std::tuple<int, bool, bool> Game::getResult(int offset, const Player& player) const
+{
+    std::cout << player.name() << " ";
+    std::cout << &player << " " << bidder_ << " " << challenger() << std::endl;
+    if (&player == bidder_)
+        return std::make_tuple(offset < 0 ? offset : 0, offset >= 0, offset < 0);
+    if (&player == challenger())
+        return std::make_tuple(offset >= 0 ? std::min(-1, -offset) : 0, offset < 0, offset >= 0);
+    return std::make_tuple(offset == 0 ? -1 : 0, false, false);
+}
+
+void Game::setTurn(const Player& player)
+{
+    std::cout << "Set turn " << player.name() << std::endl;
+    int i = 0;
+    for (const auto& p : players_)
+    {
+        if (&p == &player)
+        {
+            turn_ = i;
+            return;
+        }
+        ++i;
+    }
+}
+
+Game::Game(const std::string& game, const IDice& diceRoll)
+    : game_{game},
+    players_{},
+    round_{},
+    turn_{0},
+    currentBid_{},
+    roundStarted_{false},
+    diceRoll_{diceRoll},
+    state_{GAME_NOT_STARTED}
+{
+}
+
+void Game::addPlayer(const std::string& player)
+{
+    players_.push_back({player, diceRoll_});
+}
+
+bool Game::startGame()
+{
+    if (state_ != GAME_NOT_STARTED && state_ != GAME_FINISHED)
+        return false;
+    state_ = GAME_STARTED;
+    return true;
+}
+
+bool Game::startRound()
+{
+    std::cout << "startRound " << toString(state_) << std::endl;
+    switch (state_)
+    {
+    case CHALLENGE:
+    {
+        const auto offset = getOffset();
+        for (auto& p : players_)
+        {
+            auto result = getResult(offset, p);
+            p.remove(-std::get<0>(result));
+        }
+    }
+    //[[clang::fallthrough]];
+    case GAME_STARTED:
+        state_ = ROUND_STARTED;
+        currentBid_ = Bid{};
+        for (auto& p : players_) p.roll();
+        return true;
+    default:
+        return false;
+    }
+}
+
+void Game::nextPlayer()
+{
+    ++turn_;
+    auto nPlayers = players_.size();
+    for (size_t i = 0; i < players_.size(); ++turn_)
+    {
+        turn_ %= nPlayers;
+        if (players_[turn_].isPlaying()) return;
+    }
+    assert(false);
+}
+
+RetVal Game::bid(const std::string& player, int n, int face)
+{
+    if (state_ != ROUND_STARTED) return Error{"ROUND_NOT_STARTED"};
+    if (player != currentPlayer().name())
+    {
+        return Error{"NOT_YOUR_TURN"};
+            //json::Json{"turn", currentPlayer().name()}};
+    }
+    Bid bid{n, face};
+    if (!(currentBid_ < bid)) return Error{"TOO_LOW_BID"};
+
+    currentBid_ = bid;
+    //@TODO set bit to player
+    bidder_ = &currentPlayer();
+    currentPlayer().bid(bid);
+    nextPlayer();
+    return Success{};
+}
+
+RetVal Game::challenge(const std::string player)
+{
+    if (currentBid_ == Bid{}) return Error{"NOTHING_TO_CHALLENGE"};
+    if (player != currentPlayer().name()) return Error{"NOT_YOUR_TURN"};
+    
+    const auto offset = getOffset();
+    
+    // Is it done...
+    int numPlayers = 0;
+    for (const auto& p : players_)
+    {
+        auto result = getResult(offset, p);
+        //std::cout << p.name() << " " << p.hand().size() << " "
+        if (p.hand().size() > -std::get<0>(result)) ++numPlayers;
+    }
+    assert(numPlayers >= 1);
+    state_ = numPlayers == 1 ? GAME_FINISHED : CHALLENGE;
+    
+    challenger_ = &currentPlayer();
+    if (offset >= 0)
+    {
+        setTurn(*bidder_);
+    }
+    return Success{};
+}
+
+std::string Game::getStatus(const std::string& player)
+{
+    rapidjson::StringBuffer s;
+    rapidjson::PrettyWriter<rapidjson::StringBuffer> w{s};
+    w.StartObject();
+    w.Key("turn"); w.String(currentPlayer().name().c_str());
+    w.Key("state"); w.String(toString(state_));
+
+    w.Key("players");
+    w.StartArray();
+    
+    const auto offset = getOffset();
+    for (const auto& p : players_)
+    {
+        if (state_ == CHALLENGE || state_ == GAME_FINISHED)
+        {
+            auto result = getResult(offset, p);
+            std::cout << p.name() << " " << offset << " " << std::get<0>(result) << std::endl;
+            p.serialize(w, result);
+        }
+        else
+        {
+            p.serialize(w, player);
+        }
+    }
+    w.EndArray();
+    w.EndObject();
+    return s.GetString();
+}
+
+/**
+ * Serialize engine state to give writer. If round is still in progress,
+ * only given player's dice are "shown".
+ *
+ * @param w [out] state is serialized here
+ * @param name [in] who's dice to show if round is in progress. If empty,
+ *     show all dice.
+ */
+void Game::serialize(rapidjson::PrettyWriter<rapidjson::StringBuffer>& w, const std::string& name) const
+{
+    w.StartObject();
+    w.Key("game");
+    w.String(game_.c_str());
+
+    w.Key("state");
+    w.String(toString(state_));
+
+    w.Key("turn");
+    std::cout << "turn: " << turn_ << std::endl;
+    w.Int(turn_);
+
+    w.Key("bid");
+    w.StartObject();
+    w.Key("n");
+    w.Int(currentBid_.n());
+    w.Key("face");
+    w.Int(currentBid_.face());
+    w.EndObject();
+
+    w.Key("players");
+    w.StartArray();
+    if (state_ == CHALLENGE || state_ == GAME_FINISHED)
+    {
+        const auto offset = getOffset();
+        for (const auto& p : players_)
+        {
+            auto result = getResult(offset, p);
+            p.serialize(w, result);
+        }
+    }
+    else
+        for (const auto& p : players_)
+            p.serialize(w, name);
+    w.EndArray();
+    w.EndObject();
+}
+
+std::shared_ptr<Game> Game::fromJson(const rapidjson::Value& v)
+{
+    std::cout << "ParseGame" << std::endl;
+    if (v.IsObject() &&
+        v.HasMember("game") && v["game"].IsString() &&
+        v.HasMember("turn") && v["turn"].IsInt() &&
+        v.HasMember("state") && v["state"].IsString() &&
+        v.HasMember("bid") && v["bid"].IsObject() &&
+        v.HasMember("players") && v["players"].IsArray())
+    {
+        auto game = std::make_shared<Game>(v["game"].GetString());
+        game->turn_ = v["turn"].GetInt();
+        game->state_ = fromString(v["state"].GetString());
+        game->currentBid_ = Bid::fromJson(v["bid"]);
+        for (const auto& jplayer : v["players"].GetArray())
+        {
+            auto player = Player::fromJson(jplayer);
+            if (player)
+            {
+                std::cout << "Valid" << std::endl;
+                game->players_.push_back(player);
+            }
+            else
+            {
+                std::cout << "Invalid" << std::endl;
+            }
+        }
+        return game;
+    }
+    
+    return std::shared_ptr<Game>{};
+}
+
+} // namespace dice
--- game.hpp
+++ game.hpp
@@ -43,213 +43,34 @@ class Game
     const Player* bidder_;
     const Player* challenger_;
     
-    auto getOffset() const
-    {
-        //if (state_ == CHALLENGE)
-        {
-            std::vector<int> commonHand;
-            for (const auto p : players_)
-            {
-                commonHand.insert(commonHand.end(), p.hand().begin(), p.hand().end());
-            }
-            return currentBid_.challenge(commonHand);
-        }
-        return 0;
-    }
+    int getOffset() const;
 
-    static const char* toString(State state)
-    {
-        switch (state)
-        {
-        case GAME_NOT_STARTED: return "GAME_NOT_STARTED";
-        case GAME_STARTED: return "GAME_STARTED";
-        case ROUND_STARTED: return "ROUND_STARTED";
-        case CHALLENGE: return "CHALLENGE";
-        case GAME_FINISHED: return "GAME_FINISHED";
-        }
-    }
-    static auto fromString(const std::string& str)
-    {
-        if (str == "GAME_NOT_STARTED") return GAME_NOT_STARTED;
-        if (str == "GAME_STARTED") return GAME_STARTED;
-        if (str == "ROUND_STARTED") return ROUND_STARTED;
-        if (str == "CHALLENGE") return CHALLENGE;
-        if (str == "GAME_FINISHED") return GAME_FINISHED;
-        return GAME_NOT_STARTED;
-    }
+    static const char* toString(State state);
+    static State fromString(const std::string& str);
     
     auto& currentPlayer() { return players_[turn_]; }
     const auto& currentPlayer() const { return players_[turn_]; }
-    
     const auto challenger() const { return challenger_; }
     
-    auto getResult(int offset, const Player& player) const
-    {
-        std::cout << player.name() << " ";
-        std::cout << &player << " " << bidder_ << " " << challenger() << std::endl;
-        if (&player == bidder_)
-            return std::make_tuple(offset < 0 ? offset : 0, offset >= 0, offset < 0);
-        if (&player == challenger())
-            return std::make_tuple(offset >= 0 ? std::min(-1, -offset) : 0, offset < 0, offset >= 0);
-        return std::make_tuple(offset == 0 ? -1 : 0, false, false);
-    }
+    std::tuple<int, bool, bool> getResult(int offset, const Player& player) const;
     
-    void setTurn(const Player& player)
-    {
-        std::cout << "Set turn " << player.name() << std::endl;
-        int i = 0;
-        for (const auto& p : players_)
-        {
-            if (&p == &player)
-            {
-                turn_ = i;
-                return;
-            }
-            ++i;
-        }
-    }
+    void setTurn(const Player& player);
 
 public:
-    Game(const std::string& game, const IDice& diceRoll = Dice::instance())
-      : game_{game},
-        players_{},
-        round_{},
-        turn_{0},
-        currentBid_{},
-        roundStarted_{false},
-        diceRoll_{diceRoll},
-        state_{GAME_NOT_STARTED}
-    {
-    }
+    Game(const std::string& game, const IDice& diceRoll = Dice::instance());
 
     const auto& name() { return game_; }
-
-    void addPlayer(const std::string& player)
-    {
-        players_.push_back({player, diceRoll_});
-    }
+    void addPlayer(const std::string& player);
 
     auto players() const { return players_; }
     
-    bool startGame()
-    {
-        if (state_ != GAME_NOT_STARTED && state_ != GAME_FINISHED)
-            return false;
-        state_ = GAME_STARTED;
-        return true;
-    }
+    bool startGame();
     
-    bool startRound()
-    {
-        std::cout << "startRound " << toString(state_) << std::endl;
-        switch (state_)
-        {
-        case CHALLENGE:
-        {
-            const auto offset = getOffset();
-            for (auto& p : players_)
-            {
-                auto result = getResult(offset, p);
-                p.remove(-std::get<0>(result));
-            }
-        }
-        //[[clang::fallthrough]];
-        case GAME_STARTED:
-            state_ = ROUND_STARTED;
-            currentBid_ = Bid{};
-            for (auto& p : players_) p.roll();
-            return true;
-        default:
-            return false;
-        }
-    }
-
-    void nextPlayer()
-    {
-        ++turn_;
-        auto nPlayers = players_.size();
-        for (size_t i = 0; i < players_.size(); ++turn_)
-        {
-            turn_ %= nPlayers;
-            if (players_[turn_].isPlaying()) return;
-        }
-        assert(false);
-    }
-    
-    RetVal bid(const std::string& player, int n, int face)
-    {
-        if (state_ != ROUND_STARTED) return Error{"ROUND_NOT_STARTED"};
-        if (player != currentPlayer().name())
-        {
-            return Error{"NOT_YOUR_TURN"};
-                //json::Json{"turn", currentPlayer().name()}};
-        }
-        Bid bid{n, face};
-        if (!(currentBid_ < bid)) return Error{"TOO_LOW_BID"};
-
-        currentBid_ = bid;
-        //@TODO set bit to player
-        bidder_ = &currentPlayer();
-        currentPlayer().bid(bid);
-        nextPlayer();
-        return Success{};
-    }
-    
-    RetVal challenge(const std::string player)
-    {
-        if (currentBid_ == Bid{}) return Error{"NOTHING_TO_CHALLENGE"};
-        if (player != currentPlayer().name()) return Error{"NOT_YOUR_TURN"};
-        
-        const auto offset = getOffset();
-        
-        // Is it done...
-        int numPlayers = 0;
-        for (const auto& p : players_)
-        {
-            auto result = getResult(offset, p);
-            //std::cout << p.name() << " " << p.hand().size() << " "
-            if (p.hand().size() > -std::get<0>(result)) ++numPlayers;
-        }
-        assert(numPlayers >= 1);
-        state_ = numPlayers == 1 ? GAME_FINISHED : CHALLENGE;
-        
-        challenger_ = &currentPlayer();
-        if (offset >= 0)
-        {
-            setTurn(*bidder_);
-        }
-        return Success{};
-    }
-    
-    std::string getStatus(const std::string& player)
-    {
-        rapidjson::StringBuffer s;
-        rapidjson::PrettyWriter<rapidjson::StringBuffer> w{s};
-        w.StartObject();
-        w.Key("turn"); w.String(currentPlayer().name().c_str());
-        w.Key("state"); w.String(toString(state_));
-
-        w.Key("players");
-        w.StartArray();
-        
-        const auto offset = getOffset();
-        for (const auto& p : players_)
-        {
-            if (state_ == CHALLENGE || state_ == GAME_FINISHED)
-            {
-                auto result = getResult(offset, p);
-                std::cout << p.name() << " " << offset << " " << std::get<0>(result) << std::endl;
-                p.serialize(w, result);
-            }
-            else
-            {
-                p.serialize(w, player);
-            }
-        }
-        w.EndArray();
-        w.EndObject();
-        return s.GetString();
-    }
+    bool startRound();
+    void nextPlayer();
+    RetVal bid(const std::string& player, int n, int face);
+    RetVal challenge(const std::string player);
+    std::string getStatus(const std::string& player);
 
     /**
      * Serialize engine state to give writer. If round is still in progress,
@@ -259,77 +80,8 @@ public:
      * @param name [in] who's dice to show if round is in progress. If empty,
      *     show all dice.
      */
-    void serialize(rapidjson::PrettyWriter<rapidjson::StringBuffer>& w, const std::string& name) const
-    {
-        w.StartObject();
-        w.Key("game");
-        w.String(game_.c_str());
-
-        w.Key("state");
-        w.String(toString(state_));
-
-        w.Key("turn");
-        std::cout << "turn: " << turn_ << std::endl;
-        w.Int(turn_);
-
-        w.Key("bid");
-        w.StartObject();
-        w.Key("n");
-        w.Int(currentBid_.n());
-        w.Key("face");
-        w.Int(currentBid_.face());
-        w.EndObject();
-
-        w.Key("players");
-        w.StartArray();
-        if (state_ == CHALLENGE || state_ == GAME_FINISHED)
-        {
-            const auto offset = getOffset();
-            for (const auto& p : players_)
-            {
-                auto result = getResult(offset, p);
-                p.serialize(w, result);
-            }
-        }
-        else
-            for (const auto& p : players_)
-                p.serialize(w, name);
-        w.EndArray();
-        w.EndObject();
-    }
-
-    static auto fromJson(const rapidjson::Value& v)
-    {
-        std::cout << "ParseGame" << std::endl;
-        if (v.IsObject() &&
-            v.HasMember("game") && v["game"].IsString() &&
-            v.HasMember("turn") && v["turn"].IsInt() &&
-            v.HasMember("state") && v["state"].IsString() &&
-            v.HasMember("bid") && v["bid"].IsObject() &&
-            v.HasMember("players") && v["players"].IsArray())
-        {
-            auto game = std::make_shared<Game>(v["game"].GetString());
-            game->turn_ = v["turn"].GetInt();
-            game->state_ = fromString(v["state"].GetString());
-            game->currentBid_ = Bid::fromJson(v["bid"]);
-            for (const auto& jplayer : v["players"].GetArray())
-            {
-                auto player = Player::fromJson(jplayer);
-                if (player)
-                {
-                    std::cout << "Valid" << std::endl;
-                    game->players_.push_back(player);
-                }
-                else
-                {
-                    std::cout << "Invalid" << std::endl;
-                }
-            }
-            return game;
-        }
-        
-        return std::shared_ptr<Game>{};
-    }
+    void serialize(rapidjson::PrettyWriter<rapidjson::StringBuffer>& w, const std::string& name) const;
+    static std::shared_ptr<Game> fromJson(const rapidjson::Value& v);
 };
 
 } // namespace game
